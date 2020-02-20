95ccb8cbc0b7ce1957e2abcccfb527a3cab0e845
twistedgrim <james.cobb@rackspace.com>
2020-02-20 09:58:00 -0600

    reorder outputs

--- outputs.tf
+++ outputs.tf
@@ -1,13 +1,6 @@
-output "id" {
-  description = "The identifier for the distribution."
-  value = element(
-    coalescelist(
-      aws_cloudfront_distribution.cf_distribution.*.id,
-      aws_cloudfront_distribution.cf_distribution_no_s3_origin_config.*.id,
-      [""],
-    ),
-    0,
-  )
+output "active_trusted_signers" {
+  description = "The key pair IDs that CloudFront is aware of for each trusted signer, if the distribution is set up to serve private content with signed URLs."
+  value       = local.active_trusted_signers[0]
 }
 
 output "arn" {
@@ -34,52 +27,12 @@ output "caller_reference" {
   )
 }
 
-output "status" {
-  description = "The current status of the distribution."
-  value = element(
-    coalescelist(
-      aws_cloudfront_distribution.cf_distribution.*.status,
-      aws_cloudfront_distribution.cf_distribution_no_s3_origin_config.*.status,
-      [""],
-    ),
-    0,
-  )
-}
-
-output "active_trusted_signers" {
-  description = "The key pair IDs that CloudFront is aware of for each trusted signer, if the distribution is set up to serve private content with signed URLs."
-  value       = local.active_trusted_signers[0]
-}
-
 output "domain_name" {
   description = "The domain name corresponding to the distribution."
   value = element(
     coalescelist(
       aws_cloudfront_distribution.cf_distribution.*.domain_name,
       aws_cloudfront_distribution.cf_distribution_no_s3_origin_config.*.domain_name,
-      [""],
-    ),
-    0,
-  )
-}
-
-output "last_modified_time" {
-  description = "The date and time the distribution was last modified."
-  value = element(
-    coalescelist(
-      aws_cloudfront_distribution.cf_distribution.*.last_modified_time,
-      aws_cloudfront_distribution.cf_distribution_no_s3_origin_config.*.last_modified_time,
-    ),
-    0,
-  )
-}
-
-output "in_progress_validation_batches" {
-  description = "The number of invalidation batches currently in progress."
-  value = element(
-    coalescelist(
-      aws_cloudfront_distribution.cf_distribution.*.in_progress_validation_batches,
-      aws_cloudfront_distribution.cf_distribution_no_s3_origin_config.*.in_progress_validation_batches,
       [""],
     ),
     0,
@@ -110,3 +63,49 @@ output "hosted_zone_id" {
   )
 }
 
+output "id" {
+  description = "The identifier for the distribution."
+  value = element(
+    coalescelist(
+      aws_cloudfront_distribution.cf_distribution.*.id,
+      aws_cloudfront_distribution.cf_distribution_no_s3_origin_config.*.id,
+      [""],
+    ),
+    0,
+  )
+}
+
+output "in_progress_validation_batches" {
+  description = "The number of invalidation batches currently in progress."
+  value = element(
+    coalescelist(
+      aws_cloudfront_distribution.cf_distribution.*.in_progress_validation_batches,
+      aws_cloudfront_distribution.cf_distribution_no_s3_origin_config.*.in_progress_validation_batches,
+      [""],
+    ),
+    0,
+  )
+}
+
+output "last_modified_time" {
+  description = "The date and time the distribution was last modified."
+  value = element(
+    coalescelist(
+      aws_cloudfront_distribution.cf_distribution.*.last_modified_time,
+      aws_cloudfront_distribution.cf_distribution_no_s3_origin_config.*.last_modified_time,
+    ),
+    0,
+  )
+}
+
+output "status" {
+  description = "The current status of the distribution."
+  value = element(
+    coalescelist(
+      aws_cloudfront_distribution.cf_distribution.*.status,
+      aws_cloudfront_distribution.cf_distribution_no_s3_origin_config.*.status,
+      [""],
+    ),
+    0,
+  )
+}
