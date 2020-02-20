e46fb5cbb84bc9bb772675d63d9ebee48ca1aff5
twistedgrim <james.cobb@rackspace.com>
2020-02-20 09:56:00 -0600

    reorder variables

--- variables.tf
+++ variables.tf
@@ -1,19 +1,7 @@
-variable "environment" {
-  description = "Application environment for which this is being created. one of: ('Development', 'Integration', 'PreProduction', 'Production', 'QA', 'Staging', 'Test')"
+variable "acm_certificate_arn" {
+  description = "The ARN of the AWS Certificate Manager certificate that you wish to use with this distribution. Specify this, cloudfront_default_certificate, or iam_certificate_id. The ACM certificate must be in US-EAST-1."
   type        = string
-  default     = "Development"
-}
-
-variable "tags" {
-  description = "Custom tags to apply to all resources."
-  type        = map(string)
-  default     = {}
-}
-
-variable "bucket_logging" {
-  description = "Enable logging to an S3 Bucket. If this is set you must configure below."
-  type        = bool
-  default     = false
+  default     = ""
 }
 
 variable "aliases" {
@@ -22,10 +10,34 @@ variable "aliases" {
   default     = []
 }
 
-variable "ordered_cache_behavior" {
-  description = "An ordered list of cache behaviors resource for this distribution. (OPTIONAL)"
+variable "allowed_methods" {
+  description = "HTTP methods that CloudFront processes and forwards to your Amazon S3 bucket or your custom origin. Allowed values are: [\"HEAD\", \"GET\"], [\"GET\", \"HEAD\", \"OPTIONS\"], or [\"DELETE\", \"GET\", \"HEAD\", \"OPTIONS\", \"PATCH\", \"POST\", \"PUT\"]."
   type        = list(string)
-  default     = []
+  default     = ["HEAD", "GET"]
+}
+
+variable "bucket" {
+  description = "The Amazon S3 bucket address where access logs are stored"
+  type        = string
+  default     = ""
+}
+
+variable "bucket_logging" {
+  description = "Enable logging to an S3 Bucket. If this is set you must configure below."
+  type        = bool
+  default     = false
+}
+
+variable "cached_methods" {
+  description = "HTTP methods for which CloudFront caches responses. Allowed values are: [\"HEAD\", \"GET\"] or [\"GET\", \"HEAD\", \"OPTIONS\"]."
+  type        = list(string)
+  default     = ["HEAD", "GET"]
+}
+
+variable "cloudfront_default_certificate" {
+  description = "if you want viewers to use HTTPS to request your objects and you're using the CloudFront domain name for your distribution. Specify this, acm_certificate_arn, or iam_certificate_id."
+  type        = string
+  default     = ""
 }
 
 variable "comment" {
@@ -34,8 +46,48 @@ variable "comment" {
   default     = ""
 }
 
+variable "compress" {
+  description = "Indicates whether CloudFront automatically compresses certain files for this cache behavior. (OPTIONAL)"
+  type        = bool
+  default     = false
+}
+
+# Custom Error Response
+# http://docs.aws.amazon.com/AmazonCloudFront/latest/DeveloperGuide/custom-error-pages.html#custom-error-pages-procedure
+# https://www.terraform.io/docs/providers/aws/r/cloudfront_distribution.html#custom-error-response-arguments
+#
+# error_code (Required) - The 4xx or 5xx HTTP status code that you want to customize.
+# error_caching_min_ttl (Optional) - The minimum amount of time you want HTTP error codes to stay in CloudFront caches before CloudFront queries your origin to see whether the object has been updated.
+# response_code (Optional) - The HTTP status code that you want CloudFront to return with the custom error page to the viewer.
+# response_page_path (Optional) - The path of the custom error page (for example, /custom_404.html).
+# Note, response_code and response_page_path must be used together, if either is specified.
+
+variable "custom_error_response" {
+  description = "(Optional) - List of one or more custom error response element maps"
+  type        = list(map(string))
+  default     = []
+}
+
+variable "custom_header" {
+  description = "One or more sub-resources with name and value parameters that specify header data that will be sent to the origin"
+  type        = list(string)
+  default     = []
+}
+
 variable "default_root_object" {
   description = "The object that you want CloudFront to return (for example, index.html) when an end user requests the root URL."
+  type        = string
+  default     = ""
+}
+
+variable "default_ttl" {
+  description = "The default time in seconds that objects stay in CloudFront caches before CloudFront forwards another request to your custom origin to determine whether the object has been updated."
+  type        = number
+  default     = 3600
+}
+
+variable "domain_name" {
+  description = "The DNS domain name of either the S3 bucket, or web site of your custom origin."
   type        = string
   default     = ""
 }
@@ -46,10 +98,22 @@ variable "enabled" {
   default     = true
 }
 
-variable "is_ipv6_enabled" {
-  description = "Whether the IPv6 is enabled for the distribution. (OPTIONAL)"
-  type        = bool
-  default     = false
+variable "environment" {
+  description = "A field used to set the Environment tag on created resources"
+  type        = string
+  default     = "Development"
+}
+
+variable "forward" {
+  description = "pecifies whether you want CloudFront to forward cookies to the origin that is associated with this cache behavior. You can specify all, none or whitelist. If whitelist, you must include the subsequent whitelisted_names"
+  type        = string
+  default     = "all"
+}
+
+variable "headers" {
+  description = "Specifies the headers that you want Amazon CloudFront to forward to the origin for this cache behavior. (OPTIONAL)"
+  type        = list(string)
+  default     = []
 }
 
 variable "http_version" {
@@ -58,34 +122,22 @@ variable "http_version" {
   default     = "http2"
 }
 
-variable "price_class" {
-  description = "The price class for this distribution. One of PriceClass_All, PriceClass_200, PriceClass_100."
+variable "iam_certificate_id" {
+  description = "The IAM certificate identifier of the custom viewer certificate for this distribution if you are using a custom domain. Specify this, acm_certificate_arn, or cloudfront_default_certificate."
   type        = string
-  default     = "PriceClass_All"
+  default     = ""
 }
 
-variable "allowed_methods" {
-  description = "HTTP methods that CloudFront processes and forwards to your Amazon S3 bucket or your custom origin. Allowed values are: [\"HEAD\", \"GET\"], [\"GET\", \"HEAD\", \"OPTIONS\"], or [\"DELETE\", \"GET\", \"HEAD\", \"OPTIONS\", \"PATCH\", \"POST\", \"PUT\"]."
-  type        = list(string)
-  default     = ["HEAD", "GET"]
-}
-
-variable "cached_methods" {
-  description = "HTTP methods for which CloudFront caches responses. Allowed values are: [\"HEAD\", \"GET\"] or [\"GET\", \"HEAD\", \"OPTIONS\"]."
-  type        = list(string)
-  default     = ["HEAD", "GET"]
-}
-
-variable "compress" {
-  description = "Indicates whether CloudFront automatically compresses certain files for this cache behavior. (OPTIONAL)"
+variable "include_cookies" {
+  description = "Indicates whether CloudFront includes cookies in access logs."
   type        = bool
   default     = false
 }
 
-variable "default_ttl" {
-  description = "The default time in seconds that objects stay in CloudFront caches before CloudFront forwards another request to your custom origin to determine whether the object has been updated."
-  type        = number
-  default     = 3600
+variable "is_ipv6_enabled" {
+  description = "Whether the IPv6 is enabled for the distribution. (OPTIONAL)"
+  type        = bool
+  default     = false
 }
 
 # Removing this property due to issues dynamically providing these values.  Will be reenabled
@@ -96,6 +148,12 @@ variable "default_ttl" {
 #   type        = "list"
 #   default     = []
 # }
+
+variable "locations" {
+  description = "The two-letter, uppercase country code for a country that you want to include in your blacklist or whitelist."
+  type        = list(string)
+  default     = []
+}
 
 variable "max_ttl" {
   description = "The maximum amount of time (in seconds) that an object is in a CloudFront cache before CloudFront forwards another request to your origin to determine whether the object has been updated. (OPTIONAL)"
@@ -109,15 +167,92 @@ variable "min_ttl" {
   default     = 0
 }
 
+variable "minimum_protocol_version" {
+  description = "The minimum version of the SSL protocol that you want CloudFront to use for HTTPS connections. See https://www.terraform.io/docs/providers/aws/r/cloudfront_distribution.html#minimum_protocol_version"
+  type        = string
+  default     = "TLSv1.2_2018"
+}
+
+variable "ordered_cache_behavior" {
+  description = "An ordered list of cache behaviors resource for this distribution. (OPTIONAL)"
+  type        = list(string)
+  default     = []
+}
+
+variable "origin_access_identity" {
+  description = "The CloudFront origin access identity to associate with the origin. You must specify the full origin ID"
+  type        = string
+  default     = ""
+}
+
+variable "origin_access_identity_provided" {
+  description = "origin_access_identity has been provided"
+  type        = bool
+  default     = false
+}
+
+variable "origin_id" {
+  description = "An identifier for the origin. The value of Id must be unique within the distribution."
+  type        = string
+}
+
+variable "origin_path" {
+  description = "The path that CloudFront uses to request content from an S3 bucket or custom origin. The combination of the DomainName and OriginPath properties must resolve to a valid path. The value must start with a slash mark (/) and cannot end with a slash mark. (OPTIONAL)"
+  type        = string
+  default     = ""
+}
+
 variable "path_pattern" {
   description = "The pattern to which an ordered cache behavior applies."
   type        = string
+}
+
+variable "prefix" {
+  description = "Indicates whether CloudFront includes cookies in access logs."
+  type        = string
+  default     = ""
+}
+
+variable "price_class" {
+  description = "The price class for this distribution. One of PriceClass_All, PriceClass_200, PriceClass_100."
+  type        = string
+  default     = "PriceClass_All"
+}
+
+variable "query_string" {
+  description = "Indicates whether you want CloudFront to forward query strings to the origin that is associated with this cache behavior."
+  type        = bool
+  default     = false
+}
+
+variable "query_string_cache_keys" {
+  description = "When specified, along with a value of true for query_string, all query strings are forwarded, however only the query string keys listed in this argument are cached. When omitted with a value of true for query_string, all query string keys are cached. (OPTIONAL)"
+  type        = list(string)
+  default     = []
+}
+
+variable "restriction_type" {
+  description = "The method that you want to use to restrict distribution of your content by country: none, whitelist, or blacklist."
+  type        = string
+  default     = ""
 }
 
 variable "smooth_streaming" {
   description = "Indicates whether you want to distribute media files in Microsoft Smooth Streaming format using the origin that is associated with this cache behavior. (OPTIONAL)"
   type        = bool
   default     = false
+}
+
+variable "ssl_support_method" {
+  description = "Specifies how you want CloudFront to serve HTTPS requests. One of vip or sni-only. Required if you specify acm_certificate_arn or iam_certificate_id. NOTE: vip causes CloudFront to use a dedicated IP address and may incur extra charges."
+  type        = string
+  default     = "sni-only"
+}
+
+variable "tags" {
+  description = "A mapping of tags applied to resources created by the module"
+  type        = map(string)
+  default     = {}
 }
 
 variable "target_origin_id" {
@@ -136,150 +271,14 @@ variable "viewer_protocol_policy" {
   type        = string
 }
 
-variable "forward" {
-  description = "pecifies whether you want CloudFront to forward cookies to the origin that is associated with this cache behavior. You can specify all, none or whitelist. If whitelist, you must include the subsequent whitelisted_names"
-  type        = string
-  default     = "all"
-}
-
-variable "whitelisted_names" {
-  description = "If you have specified whitelist to forward, the whitelisted cookies that you want."
-  type        = list(string)
-  default     = []
-}
-
-variable "headers" {
-  description = "Specifies the headers that you want Amazon CloudFront to forward to the origin for this cache behavior. (OPTIONAL)"
-  type        = list(string)
-  default     = []
-}
-
-variable "query_string" {
-  description = "Indicates whether you want CloudFront to forward query strings to the origin that is associated with this cache behavior."
-  type        = bool
-  default     = false
-}
-
-variable "query_string_cache_keys" {
-  description = "When specified, along with a value of true for query_string, all query strings are forwarded, however only the query string keys listed in this argument are cached. When omitted with a value of true for query_string, all query string keys are cached. (OPTIONAL)"
-  type        = list(string)
-  default     = []
-}
-
-variable "bucket" {
-  description = "The Amazon S3 bucket address where access logs are stored"
-  type        = string
-  default     = ""
-}
-
-variable "include_cookies" {
-  description = "Indicates whether CloudFront includes cookies in access logs."
-  type        = bool
-  default     = false
-}
-
-variable "prefix" {
-  description = "Indicates whether CloudFront includes cookies in access logs."
-  type        = string
-  default     = ""
-}
-
-variable "domain_name" {
-  description = "The DNS domain name of either the S3 bucket, or web site of your custom origin."
-  type        = string
-  default     = ""
-}
-
-variable "custom_header" {
-  description = "One or more sub-resources with name and value parameters that specify header data that will be sent to the origin"
-  type        = list(string)
-  default     = []
-}
-
-variable "origin_id" {
-  description = "An identifier for the origin. The value of Id must be unique within the distribution."
-  type        = string
-}
-
-variable "origin_path" {
-  description = "The path that CloudFront uses to request content from an S3 bucket or custom origin. The combination of the DomainName and OriginPath properties must resolve to a valid path. The value must start with a slash mark (/) and cannot end with a slash mark. (OPTIONAL)"
-  type        = string
-  default     = ""
-}
-
-variable "origin_access_identity" {
-  description = "The CloudFront origin access identity to associate with the origin. You must specify the full origin ID"
-  type        = string
-  default     = ""
-}
-
-variable "origin_access_identity_provided" {
-  description = "origin_access_identity has been provided"
-  type        = bool
-  default     = false
-}
-
-variable "locations" {
-  description = "The two-letter, uppercase country code for a country that you want to include in your blacklist or whitelist."
-  type        = list(string)
-  default     = []
-}
-
-variable "restriction_type" {
-  description = "The method that you want to use to restrict distribution of your content by country: none, whitelist, or blacklist."
-  type        = string
-  default     = ""
-}
-
-variable "acm_certificate_arn" {
-  description = "The ARN of the AWS Certificate Manager certificate that you wish to use with this distribution. Specify this, cloudfront_default_certificate, or iam_certificate_id. The ACM certificate must be in US-EAST-1."
-  type        = string
-  default     = ""
-}
-
-variable "cloudfront_default_certificate" {
-  description = "if you want viewers to use HTTPS to request your objects and you're using the CloudFront domain name for your distribution. Specify this, acm_certificate_arn, or iam_certificate_id."
-  type        = string
-  default     = ""
-}
-
-variable "iam_certificate_id" {
-  description = "The IAM certificate identifier of the custom viewer certificate for this distribution if you are using a custom domain. Specify this, acm_certificate_arn, or cloudfront_default_certificate."
-  type        = string
-  default     = ""
-}
-
-variable "minimum_protocol_version" {
-  description = "The minimum version of the SSL protocol that you want CloudFront to use for HTTPS connections. See https://www.terraform.io/docs/providers/aws/r/cloudfront_distribution.html#minimum_protocol_version"
-  type        = string
-  default     = "TLSv1.2_2018"
-}
-
-variable "ssl_support_method" {
-  description = "Specifies how you want CloudFront to serve HTTPS requests. One of vip or sni-only. Required if you specify acm_certificate_arn or iam_certificate_id. NOTE: vip causes CloudFront to use a dedicated IP address and may incur extra charges."
-  type        = string
-  default     = "sni-only"
-}
-
-# WAF
 variable "web_acl_id" {
   description = "The AWS WAF web ACL to associate with this distribution."
   type        = string
   default     = ""
 }
 
-# Custom Error Response
-# http://docs.aws.amazon.com/AmazonCloudFront/latest/DeveloperGuide/custom-error-pages.html#custom-error-pages-procedure
-# https://www.terraform.io/docs/providers/aws/r/cloudfront_distribution.html#custom-error-response-arguments
-#
-# error_code (Required) - The 4xx or 5xx HTTP status code that you want to customize.
-# error_caching_min_ttl (Optional) - The minimum amount of time you want HTTP error codes to stay in CloudFront caches before CloudFront queries your origin to see whether the object has been updated.
-# response_code (Optional) - The HTTP status code that you want CloudFront to return with the custom error page to the viewer.
-# response_page_path (Optional) - The path of the custom error page (for example, /custom_404.html).
-# Note, response_code and response_page_path must be used together, if either is specified.
-
-variable "custom_error_response" {
-  description = "(Optional) - List of one or more custom error response element maps"
-  type        = list(map(string))
+variable "whitelisted_names" {
+  description = "If you have specified whitelist to forward, the whitelisted cookies that you want."
+  type        = list(string)
   default     = []
 }
